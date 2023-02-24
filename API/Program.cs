551f252638e933b50974c51b6da505c3bc67d6f0
admin0322 <admin0322@example.com>
2023-02-24 23:16:56 +0100

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 //app.UseHttpsRedirection();
 
